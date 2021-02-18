30215942d139c6813ed31a74fb7c2efe2d469204
MarkusOrvasto <markus.orvasto@gmail.com>
2021-02-18 05:09:08 +0200

    Create Program.cs
    Session 4, assignment 2C:
    My code that reads user input for distance and gives different output depending on input value.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace exercise2c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter aircraft distance to the base (number):");
+            var h = Console.ReadLine();
+            if (int.TryParse(h, out int p))
+            {
+                Tutka(p);
+            }
+            else
+                Console.WriteLine($"{h} is not number");
+        }
+
+        static void Tutka(int p)
+        {
+            int x = 0;
+            while (x < 4)
+            {
+                if (p < 100)
+                {
+                    Console.WriteLine("If you don’t leave this area immediately, we will open fire on you!");
+                    break;
+                }
+                else if ((p >= 100) && (p <= 250))
+                {
+                    Console.WriteLine("This is a restricted area, please turn away now.");
+                    x++;
+                    if (x == 3)
+                    {
+                        Console.WriteLine("If you don’t leave this area immediately, we will open fire on you!");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("radar activating");
+                    Console.WriteLine("No enemy aircraft detected near the base");
+                    break;
+                }
+            }return;
+        }
+    }
+}
